11b9cda2a39269fc27a90712364efea85caa560b
Artem Sychov <artem.suchov@gmail.com>
2019-07-18 14:49:05 +0300

    fetch the stories with the try catch and error showing and map through the stories and show the titles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,35 @@
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+
 class Index extends React.Component {
+  static async getInitialProps() {
+    let stories;
+    try {
+      const response = await fetch(
+        "https://node-hnapi.herokuapp.com/news?page=1"
+      );
+      stories = await response.json();
+    } catch (err) {
+      console.log(err);
+      stories = [];
+    }
+    return { stories };
+  }
   render() {
-    return <div>hacker next</div>;
+    const { stories } = this.props;
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
+    }
+    return (
+      <div>
+        <h1>hacker next</h1>
+        <div>
+          {stories.map(story => (
+            <h2 key={story.id}>{story.title}</h2>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
